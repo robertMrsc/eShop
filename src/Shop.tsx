1c61379cdfc9cac377d0e9cc5ad252d471b8f147
Robert <marschmv@gmail.com>
2023-04-07 22:18:20 +0300

    Added store page

--- src/Shop.tsx
+++ src/Shop.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import {useNavigate} from 'react-router-dom'
+import {useNavigate} from 'react-router-dom';
+import './styles/Shop.scss'
 
 const Shop:React.FC = () => {
-
+    const list:number[]=[1,2,3,4,5,6,7,8]
   return (
-    <div>Shop</div>
+    <div className='shop-container'>
+        <p className='product-category'>All products</p>
+        <div className="shop-content">
+            {list.map((item)=>{
+                return <article className='shop-product'>
+                    <div className='shop-product-image'>
+                    </div>
+                    <p className='shop-product-text'>Lorem ipsum, dolor sit amet consectetur adipisicing.</p>
+                </article>
+            })}
+        </div>
+    </div>
   )
 }
 
