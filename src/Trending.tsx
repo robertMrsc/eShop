e02da019b969a5cacffe47f345b4ab843a743770
Robert <marschmv@gmail.com>
2023-05-11 03:48:45 +0300

    Updated home page trending images

--- src/Trending.tsx
+++ src/Trending.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import { useProduct } from './Context/productContext';
+import { useNavigate } from 'react-router-dom';
 
 const Trending:React.FC = () => {
+  const {products}=useProduct();
+  const trendingProducts=products.slice(0, 3);
+  console.log(products);
+  console.log(trendingProducts)
+  const navigate=useNavigate();
+  const handleNavigate=(id:string)=>{
+    navigate(`/product/${id}`)
+  }
   return (
     <div className='trending-container'>
         <p className='trending-text'>Trending products</p>
         <div className='trending-flex'>
-            <div className='trending-flex-image watch '></div>
-            <div className='trending-flex-image lens '></div>
-            <div className='trending-flex-image camera '></div>
+            {trendingProducts.map((product)=>{
+              return <div onClick={()=> handleNavigate(product._id)} style={{backgroundImage:`url(${product.imageUrl})`}} className='trending-flex-image'> </div>
+            })}
         </div>
     </div>
   )
